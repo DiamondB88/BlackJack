9b15a71294b6a7d8c41d4adcc85cf57b58199a1a
Stevie Burnette <Diamond@Mitchells-MacBook-Pro.local>
2018-09-23 23:13:19 -0400

    added modal to game for about set with timer

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 $(() => {
 
+  const $openBtn = $('#about-modal');
+  const $modal = $('#modal');
+  const $closeBtn = $('#closeModal');
 
+  const openModal = () => {
+    $modal.css('display', 'block');
+}
 
+  const closeModal = () => {
+    $modal.css('display', 'none');
+}
 
+  $openBtn.on('click', openModal);
+
+  $closeBtn.on('click', closeModal);
+
+  setTimeout(openModal, 3000)
 
 })
