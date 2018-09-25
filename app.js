593b96a3c755c356be71f03e348c2095ba878517
Stevie Burnette <Diamond@Mitchells-MacBook-Pro.local>
2018-09-25 11:13:24 -0400

    creating deck

--- app.js
+++ app.js
@@ -16,6 +16,40 @@ $(() => {
 
   $closeBtn.on('click', closeModal);
 
-  setTimeout(openModal, 3000)
+  setTimeout(openModal, 2000)
 
+  const money = 1000;
+
+
+  const face = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+  const suits = ['Diamonds','Clubs','Hearts','Spades'];
+
+  const card = (name,face,suit,value) => {
+    this.name=name;
+    this.face=face;
+    this.suit=suit;
+    this.value=value;
+  }
+
+  const deck = [];
+
+  const shuffleDeck = () => {
+    deck=[];
+
+    for (var i = 0; i < face.length; i++) {
+      for (var j = 0; j < suits.length; j++) {
+        let placeHolder;
+        if (i > 9) {
+          placeHolder = new card(face[i] + 'of' + suits[j],face[i],suits[j],10 )
+        }else {
+          placeHolder = new card(face[i] + 'of' + suits[j],face[i],suits[j], i + 1)
+        }
+        deck.push(placeHolder)
+      }
+    }
+  }
+  shuffleDeck();
+
+  
 })
